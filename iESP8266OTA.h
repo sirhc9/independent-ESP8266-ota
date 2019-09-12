057e6d2001777cdd5b9f755227e3ce2be3063058
sirhc9 <37290644+sirhc9@users.noreply.github.com>
2019-09-13 08:49:37 +1000

    Update iESP8266OTA.h

--- iESP8266OTA.h
+++ iESP8266OTA.h
@@ -1,1 +1,14 @@
+#ifndef IESP8266OTA_H_
+#define IESP8266OTA_H_
+
 #include <ESP8266WebServer.h>
+//Do we need additional headers for ESP.restart() and Update.begin()?
+
+class iESP8266OTA {
+  ESP8266WebServer mWebServer;
+  public:
+    iESP8266OTA();
+    handleClient();
+};
+
+#endif /* IESP8266OTA_H_ */
